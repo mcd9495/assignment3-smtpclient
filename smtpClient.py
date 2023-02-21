2e58a855781a5ef3906637cd2ce119129dc7e9cb
Molly Doyle <mollydoyle@Mollys-MacBook-Air.local>
2023-02-21 17:48:48 -0600

    fix

--- smtpClient.py
+++ smtpClient.py
@@ -35,7 +35,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    rcptTo = 'RCPT TO:<test@nyu.edu>\r\n'
+    rcptTo = 'RCPT TO:<mcd9495@nyu.edu>\r\n'
     clientSocket.send(rcptTo.encode())
     clientSocket.recv(1024).decode()
     # Fill in end
